6ed6bda66107b8becbd11a58c85252e368d117bb
Racso1624 <fer10_lb@outlook.com>
2021-06-03 10:36:24 -0600

    Se agregan relaciones por tiempo y nutricion
    Co-authored-by: unclepete-20 <unclepete-20@users.noreply.github.com>

--- neo.py
+++ neo.py
@@ -73,6 +73,34 @@ class HelloWorldExample:
         )
         result = tx.run(query, message1=message1, message2=message2)
 
+    #para realizar la relacion entre paltillo y precio
+    def add_price_relation(self, message1, message2):
+        with self.driver.session() as session:
+            session.write_transaction(self.create_price_relation,message1,message2)
+
+    @staticmethod
+    def create_price_relation(tx, message1, message2):
+        query = (
+            "MATCH (p1:Precio {message : $message1}), (p2:Platillos {message: $message2})"
+            "CREATE (p1)-[:relacion]->(p2) "
+            "RETURN p1, p2"
+        )
+        result = tx.run(query, message1=message1, message2=message2)
+
+    #para realizar la relacion entre paltillo y nutricion
+    def add_nutricion_relation(self, message1, message2):
+        with self.driver.session() as session:
+            session.write_transaction(self.create_nutricion_relation,message1,message2)
+
+    @staticmethod
+    def create_nutricion_relation(tx, message1, message2):
+        query = (
+            "MATCH (p1:Nutricion {message : $message1}), (p2:Platillos {message: $message2})"
+            "CREATE (p1)-[:relacion]->(p2) "
+            "RETURN p1, p2"
+        )
+        result = tx.run(query, message1=message1, message2=message2)
+
 
 if __name__ == "__main__":
     greeter = HelloWorldExample("bolt://34.205.171.52:7687", "neo4j", "light-mirrors-plants")
@@ -99,7 +127,7 @@ if __name__ == "__main__":
     greeter.add_Precio("bajo")
 
     #para realizar la relacion entre el platillo y tiempo
-    for i in transaction_list:
+    '''for i in transaction_list:
         lista = str(i[2]).split()
         for j in lista:
             if(j==("rapido")):
@@ -107,9 +135,29 @@ if __name__ == "__main__":
             elif(j==("medio")):
                 greeter.add_time_relation("medio",str(i[0]))
             elif(j==("lento")):
-                greeter.add_time_relation("lento",str(i[0]))
+                greeter.add_time_relation("lento",str(i[0]))'''
 
     #para realizar la relacion entre platillo y precio
-    
+    '''for i in transaction_list:
+        lista = str(i[1]).split()
+        for j in lista:
+            if(j==("alto")):
+                greeter.add_price_relation("alto",str(i[0]))
+            elif(j==("medio")):
+                greeter.add_price_relation("medio",str(i[0]))
+            elif(j==("bajo")):
+                greeter.add_price_relation("bajo",str(i[0]))'''
+
+
+    #para realizar la relacion entre platillo y nutricion
+    for i in transaction_list:
+        lista = str(i[3]).split()
+        for j in lista:
+            if(j==("alta")):
+                greeter.add_nutricion_relation("alta",str(i[0]))
+            elif(j==("media")):
+                greeter.add_nutricion_relation("media",str(i[0]))
+            elif(j==("baja")):
+                greeter.add_nutricion_relation("baja",str(i[0]))
 
     greeter.close()
